fd728f89e564b1b4bc5203a68994d9b7ce4d49b1
Maulana Sodiqin <msdqn@outlook.com>
2024-04-12 20:58:37 +0700

    chore: learn variable

---
+++ bin/variable.dart
@@ -0,0 +1,15 @@
+const name = "Maulana";
+var age = 12;
+
+String fullName = "";
+int brithYear = 2999;
+
+void main(){
+  age = 10;
+  print(name * age);
+
+  fullName = name;
+  print(age);
+
+  print(fullName);
+}

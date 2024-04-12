3ea0a91b9f786e90cfdd4313657c1096002134c8
Maulana Sodiqin <sodiqincahyana1@gmail.com>
2024-04-12 21:09:22 +0700

    chore: add comments

--- bin/variable.dart
+++ bin/variable.dart
@@ -1,6 +1,15 @@
+// Deklarasi Variable di Dart Ada 3
+// const -> sama sekali tidak bisa di rubah, value akan di hardcode
+// final -> masih bisa di rubah value nya
+// var -> mutable value, bisa di rubah rubah sa karep mu
+
 const name = "Maulana";
 var age = 12;
 final isSameWithConst = 12;
+
+const arrayOfHobbies = ["Sepak Bola", "Modol"];
+
+final arrayOfPCParts = ["GPU", "CPU", "RAM"];
 
 String fullName = "";
 int brithYear = 2999;
